add1872d34c8a203db4d8f5710f9f74db3b0bc60
Konstantin Mukhametshin <46990495+kostya63@users.noreply.github.com>
2024-07-08 09:36:14 +0400

    Create my_sampler.py

---
+++ my_sampler.py
@@ -0,0 +1,23 @@
+import torch
+from torch import nn
+from torch.utils.data import Dataset
+from sentence_transformers import SentenceTransformer
+import pandas as pd
+
+class NLPDataset(Dataset):
+	def __init__(self, data_file, transformer = 'paraphrase-MiniLM-L6-v2', transform=None):
+		self.input_data = pd.read_csv(data_file, keep_default_na=False)
+		self.model = SentenceTransformer(transformer)
+		self.transform = transform
+										
+	def __len__(self):
+		return len(self.input_data)
+	
+	def __getitem__(self, idx):
+		label = self.input_data.iloc[idx, 4]
+		key_word = torch.from_numpy(self.model.encode(self.input_data.iloc[idx, 1]))
+		embedding = torch.from_numpy(self.model.encode(self.input_data.iloc[idx, 3]))
+		#embedding = self.model.encode(self.input_data.iloc[idx, 3])
+		embedding = torch.cat((key_word, embedding), dim=0)
+		        
+		return embedding, label
