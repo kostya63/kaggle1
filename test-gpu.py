bf70a35b945b7124f2eea2b8c06a96b8a3f84236
Konstantin Mukhametshin <konstantin.mukhametshin@gmail.com>
2024-07-09 15:28:12 +0400

    init 2

--- test-gpu.py
+++
@@ -1,5 +0,0 @@
-import torch
-
-#device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-cuda0 = torch.device('cuda:0')
-x = torch.ones([10000, 10000], dtype=torch.float64, device=cuda0)
